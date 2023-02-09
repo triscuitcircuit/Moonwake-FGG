0680d5a4b1e91bc7ae800a98be4977aa8bee686a
JacquesLJT <me@landonthibodeau.com>
2023-02-08 21:40:07 -0500

    Working on creature creation page

--- fgg-front-end/src/pages/creature-creator.tsx
+++ fgg-front-end/src/pages/creature-creator.tsx
@@ -1,28 +1,73 @@
-import React from "react"
+import React, { useState } from "react"
 import {
     Text,
     NextUIProvider,
     createTheme,
+    Container,
+    Grid,
 } from "@nextui-org/react";
+import { StartNew, Auto } from "../components/creature-creator-cards";
 
 const theme = createTheme({
     type: "dark",
 });
 
-const CreatureCreator = () => {
+interface StartNewProps {
+    onButtonClick: () => void;
+}
+
+const CreatureCreator: React.FC = () => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    const handleButtonClick = () => {
+        setIsClicked(!isClicked);
+    }
+
     return (
         <NextUIProvider theme={theme}>
-            <Text
-                h1
-                size={60}
-                css={{
-                    textGradient: "45deg, $blue600 -20%, $pink600 50%",
-                    marginLeft: "50"
-                }}
-                weight="bold"
+            <Container
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                    border: "1px solid white",
+                    }}
             >
-                Creature Creation Page
-            </Text>
+                <Text h1>Creature Creation Page</Text>
+            </Container>
+            <Container
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                    border: "1px solid white",
+                    marginTop: "5%",
+                    }}
+            >
+                <Grid.Container
+                    gap={2}
+                    justify="center"
+                    alignItems="center"
+                >
+                    <Grid xs={12} sm={4}>
+                        {!isClicked && <StartNew onButtonClick={handleButtonClick} />}
+                        {isClicked && <p>Button clicked</p> }
+                    </Grid>
+                    <Text
+                        h2
+                        style={{
+                                margin: "5%",
+                            }}
+                    >
+                        OR
+                    </Text>
+                    <Grid xs={12} sm={4}>
+                        <Auto />
+                    </Grid>
+                </Grid.Container>
+            </Container>
         </NextUIProvider>
     );
 };
