30c47897c5dd074b11a91d36c585a20633536980
JacquesLJT <me@landonthibodeau.com>
2023-02-09 21:00:01 -0500

    Work on the creature creation form

--- fgg-front-end/src/pages/creature-creator.tsx
+++ fgg-front-end/src/pages/creature-creator.tsx
@@ -5,6 +5,10 @@ import {
     createTheme,
     Container,
     Grid,
+    Button,
+    Input,
+    Card,
+    Dropdown,
 } from "@nextui-org/react";
 import { StartNew, Auto } from "../components/creature-creator-cards";
 
@@ -32,42 +36,224 @@ const CreatureCreator: React.FC = () => {
                     alignItems: "center",
                     width: "100%",
                     border: "1px solid white",
+                    marginTop: "5%",
                     }}
             >
                 <Text h1>Creature Creation Page</Text>
             </Container>
-            <Container
-                style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    width: "100%",
-                    border: "1px solid white",
-                    marginTop: "5%",
-                    }}
-            >
-                <Grid.Container
-                    gap={2}
-                    justify="center"
-                    alignItems="center"
+            {isClicked &&
+                <Container
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        width: "100%",
+                        border: "1px solid white",
+                        marginTop: "5%",
+                        }}
                 >
-                    <Grid xs={12} sm={4}>
-                        {!isClicked && <StartNew onButtonClick={handleButtonClick} />}
-                        {isClicked && <p>Button clicked</p> }
-                    </Grid>
-                    <Text
-                        h2
+                    <Grid.Container
+                        gap={2}
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <Grid xs={12} sm={4}>
+                            <StartNew onButtonClick={handleButtonClick} />
+                        </Grid>
+                        <Text
+                            h2
+                            style={{
+                                    margin: "5%",
+                                }}
+                        >
+                            OR
+                        </Text>
+                        <Grid xs={12} sm={4}>
+                            <Auto />
+                        </Grid>
+                    </Grid.Container>
+                </Container>
+            }
+            {!isClicked &&
+                <Container>
+                    <Button onClick={handleButtonClick}>Go Back</Button>
+                    <div
                         style={{
-                                margin: "5%",
+                            display: "flex",
+                            width: "100%",
+                            border: "1px solid white",
+                            justifyContent: "center",
+                            alignItems: "center",
                             }}
                     >
-                        OR
-                    </Text>
-                    <Grid xs={12} sm={4}>
-                        <Auto />
-                    </Grid>
-                </Grid.Container>
-            </Container>
+                        <Input
+                            placeholder="Creature Name"
+                            size="xl"
+                            width="50%"
+                        />
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexWrap: "wrap",
+                            width: "100%",
+                            border: "1px solid white",
+                        }}
+                    >
+                        <div
+                            style={{
+                                display: "flex",
+                                width: "50%",
+                                border: "1px solid white",
+                                justifyContent: "flex-end",
+                                marginTop: "1%",
+                                }}
+                        >
+                            <Input
+                                placeholder="Tags (Size, Type, etc.)"
+                                size="xl"
+                                width="50%"
+                            />
+                        </div>
+                        <div
+                            style={{
+                                display: "flex",
+                                width: "50%",
+                                justifyContent: "flex-start",
+                                marginTop: "1%",
+                                border: "1px solid white"
+                            }}
+                        >
+                            <div
+                                style={{
+                                    display: "flex",
+                                    width: "50%",
+                                    border: "1px solid white",
+                                    justifyContent: "space-evenly",
+                                    }}
+                            >
+                                <Input
+                                    placeholder="Armor Class"
+                                    size="xl"
+                                    width="40%"
+                                />
+                                <Input
+                                    placeholder="Health Points"
+                                    size="xl"
+                                    width="40%"
+                                />
+                            </div>
+                        </div>
+                        <div
+                            style={{
+                                display: "flex",
+                                width: "100%",
+                                marginTop: "1%",
+                                justifyContent: "center",
+                                border: "1px solid white"
+                            }}
+                        >
+                            <div
+                                style={{
+                                    width: "50%"
+                                }}
+                            >
+                                <Text h3>Attributes</Text>
+                            </div>
+                        </div>
+                        <div
+                            style={{
+                                display: "flex",
+                                width: "100%",
+                                marginTop: "1%",
+                                justifyContent: "center",
+                                border: "1px solid white"
+                            }}
+                        >
+                            <Grid.Container gap={2} justify="space-evenly" css={{ width: "50%"}}>
+                                <Grid>
+                                    <Dropdown>
+                                        <Dropdown.Button flat>STR</Dropdown.Button>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </Grid>
+                                <Grid>
+                                    <Dropdown>
+                                        <Dropdown.Button flat>STR</Dropdown.Button>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </Grid>
+                                <Grid>
+                                    <Dropdown>
+                                        <Dropdown.Button flat>STR</Dropdown.Button>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </Grid>
+                                <Grid>
+                                    <Dropdown>
+                                        <Dropdown.Button flat>STR</Dropdown.Button>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </Grid>
+                                <Grid>
+                                    <Dropdown>
+                                        <Dropdown.Button flat>STR</Dropdown.Button>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </Grid>
+                                <Grid>
+                                    <Dropdown>
+                                        <Dropdown.Button flat>STR</Dropdown.Button>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                            <Dropdown.Item>Example</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </Grid>
+                            </Grid.Container>
+                        </div>
+                    </div>
+                </Container>
+            }
         </NextUIProvider>
     );
 };
