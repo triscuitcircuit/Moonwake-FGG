d8eb91ee9d047d954c5ee1cc29e2de9a02a2dcf7
triscuitcircuit <54428989+Triscuit-circuit@users.noreply.github.com>
2023-02-09 23:24:19 -0500

    added initial testing of database and initial connection

---
+++ fgg-back-end/tests/routes.test.js
@@ -0,0 +1,96 @@
+const request = require('supertest')
+const app = require('../src/server')
+const {response} = require("express");
+
+describe('Show all AB_ATTRIBUTE', () => {
+    it('shows all attributes', async () => {
+        await request('/api/ab_attribute', function (response) {
+            expect(response.statusCode).toEqual(200)
+            expect(response.body).toHaveProperty('AB_IDGASY_ID')
+            expect(response.body).toHaveProperty('AB_NAME')
+            expect(response.body).toHaveProperty('AB_DETAIL')
+            expect(response.body).toHaveProperty('AB_DISPLAY_NAME')
+            expect(response.body).toHaveProperty('AB_VALUE')
+            expect(response.body).toHaveProperty('AB_BONUS_VALUE')
+            expect(response.body).toHaveProperty('AB_ORDER_VALUE')
+            expect(response.body).toHaveProperty('ST_CODE')
+        })
+    })
+})
+
+describe('Show all AB_TEMP', () => {
+    it('shows all ab temp', async () => {
+        await request('/api/ab_temp_save', function (response) {
+            expect(response.statusCode).toEqual(200)
+            expect(response.body).toHaveProperty('AB_ID')
+            expect(response.body).toHaveProperty('AB_NAME')
+            expect(response.body).toHaveProperty('AB_DETAIL')
+            expect(response.body).toHaveProperty('AB_DISPLAY_NAME')
+            expect(response.body).toHaveProperty('AB_VALUE')
+            expect(response.body).toHaveProperty('AB_BONUS_VALUE')
+            expect(response.body).toHaveProperty('AB_ORDER_VALUE')
+            expect(response.body).toHaveProperty('ST_CODE')
+            expect(response.body).toHaveProperty('LAST_MODIFIED_DATE')
+            expect(response.body).toHaveProperty('LAST_MODIFIED_BY')
+            expect(response.body).toHaveProperty('ABCA_ID')
+        })
+    })
+})
+describe('Show all ABCA_ATTRIBUTE_CATEGORY', () => {
+    it('shows all abca attribute', async () => {
+        await request('/api/ABCA_ATTRIBUTE_CATEGORY', function (response) {
+            expect(response.statusCode).toEqual(200)
+
+            expect(response.body).toHaveProperty('ABCA_ID')
+            expect(response.body).toHaveProperty('ABCA_NAME')
+            expect(response.body).toHaveProperty('ABCA_SHORT_NAME')
+            expect(response.body).toHaveProperty('ABCA_DETAIL')
+            expect(response.body).toHaveProperty('ABCA_DISPLAY_NAME')
+            expect(response.body).toHaveProperty('ST_CODE')
+            expect(response.body).toHaveProperty('LAST_MODIFIED_DATE')
+            expect(response.body).toHaveProperty('LAST_MODIFIED_BY')
+            expect(response.body).toHaveProperty('GASY_ID')
+
+        })
+    })
+})
+
+
+describe('Shows all ak_attack', () => {
+    it('should show all users', async () => {
+        await request('/api/ak_attack', function (response) {
+            expect(response.statusCode).toEqual(200)
+
+            expect(response.body).toHaveProperty('AK_ID')
+            expect(response.body).toHaveProperty('AT_ID')
+            expect(response.body).toHaveProperty('AKTY_ID')
+            expect(response.body).toHaveProperty('AKCA_ID')
+            expect(response.body).toHaveProperty('PRIMARY_DATY_ID')
+            expect(response.body).toHaveProperty('AK_RELATED_ABCA_ID')
+            expect(response.body).toHaveProperty('AK_ORDER_VALUE')
+            expect(response.body).toHaveProperty('ST_CODE')
+            expect(response.body).toHaveProperty('LAST_MODIFIED_DATE')
+            expect(response.body).toHaveProperty('LAST_MODIFIED_BY')
+            expect(response.body).toHaveProperty('GASY_ID')
+
+        })
+    })
+})
+//
+describe('Shows all akca-attack-category', () => {
+    it('should show akca attack', async () => {
+        await request('/api/akca_attack_category', function (response) {
+            expect(response.statusCode).toEqual(200)
+
+            expect(response.body).toHaveProperty('AKCA_ID')
+            expect(response.body).toHaveProperty('AKCA_NAME')
+            expect(response.body).toHaveProperty('AKCA_DETAIL')
+            expect(response.body).toHaveProperty('AKCA_DISPLAY_NAME')
+            expect(response.body).toHaveProperty('ST_CODE')
+            expect(response.body).toHaveProperty('LAST_MODIFIED_DATE')
+            expect(response.body).toHaveProperty('LAST_MODIFIED_BY')
+            expect(response.body).toHaveProperty('AKCA_GASY_ID')
+
+        })
+    })
+})
