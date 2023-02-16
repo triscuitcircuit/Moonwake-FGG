afe500924a2d4058929cb458342259b269642884
Matthew Virgin <42557195+mvirgin@users.noreply.github.com>
2023-02-16 04:03:24 -0500

    Progress on log in with Google

--- fgg-front-end/src/components/Home-Demo.tsx
+++ fgg-front-end/src/components/Home-Demo.tsx
@@ -8,17 +8,97 @@ import {
     Grid,
     NextUIProvider,
     createTheme,
-    Pagination
+    Pagination,
+    User,
+    Navbar,
+    Button
 } from "@nextui-org/react";
 import {Card1, Card2, Card3} from "../Display/Card";
+import { useEffect, useRef, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 const theme = createTheme({
     type: "dark",
 });
 
+// getUserDataGoogle
+// pulls user data from user's google account
+const getUserDataGoogle = async (accessToken: string) => {
+    const { data } = await axios.get(`http://localhost:3001/api/google/userData?accessToken=${accessToken}`, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+    return data
+}
+
+// UserdataGoogle
+// holds information of user's name, picture, and email
+interface UserdataGoogle {
+    name: string
+    picture: string
+    email: string
+}
+
 const HomeDemo = () => {
+
+    const [userDataGoogle, setUserDataGoogle] = useState<null | UserdataGoogle>(null)
+
+    // loginWith
+    // gets the service user used to log in (Google only atm)
+    const loginWith = useRef(localStorage.getItem("loginWith"))
+
+    const navigate = useNavigate()
+
+    // get and set user's Google user data
+    useEffect(() => {
+        const accessToken = localStorage.getItem("accessToken")
+
+        if (accessToken && loginWith.current === "Google") {
+            getUserDataGoogle(accessToken).then(resp => {
+                setUserDataGoogle(resp)
+            })
+        }
+    }, [loginWith])
+
+    // setLogOut
+    // Log the user out, removing tokens from local storage
+    const setLogOut = () => {
+        localStorage.removeItem("accessToken")
+        localStorage.removeItem("loginWith")
+        // could use navigate("path") to force user back to screen upon logout
+    }
+
+    // if (!userDataGoogle) return null
+
     return (
         <NextUIProvider theme={theme}>
+            <Navbar isBordered variant = 'sticky'>
+                <Navbar.Brand>
+                    <User
+                        name={userDataGoogle?.name}
+                        src={userDataGoogle?.picture}
+                        description={userDataGoogle?.email}
+                        bordered
+                        color = 'primary'
+                        size = 'lg'
+                    />
+                </Navbar.Brand>
+                <Navbar.Content>
+                    <Navbar.Item>
+                        <Button
+                            auto
+                            flat
+                            size='sm'
+                            color='primary'
+                            onClick={() => setLogOut()}
+                        >
+                            Log out
+                        </Button>
+                    </Navbar.Item>
+                </Navbar.Content>
+            </Navbar>
             <Container justify="center">
                 <Grid.Container gap={2} justify="center">
                     <Grid xs={12} sm={4}>
--- fgg-front-end/src/main.tsx
+++ fgg-front-end/src/main.tsx
@@ -1,13 +1,37 @@
-// Creates the main page. App defined in App.tsx from /Display
-
 import React from "react";
 import App from "./Display/App";
-import {createRoot} from "react-dom/client";
+//import {createRoot} from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google"
+import ReactDOM from "react-dom/client"
+import {
+     NextUIProvider,
+     createTheme,
+} from "@nextui-org/react";
 
+// creates a dark theme using nextUI
+const darkTheme = createTheme({
+     type: "dark",
+});
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// this is only temporary for simple google sign in
+// for the real deal we probably want to import the value from .env like back-end
+// see info here: https://console.cloud.google.com/getting-started?authuser=4&project=winged-hue-378023
+// moonwakefgg@gmail.com
+// cApsTone2023
+const GOOGLE_CLIENT_ID = "425517943988-kp6g5nctrem3pnnf83b5p8hti86g8hij.apps.googleusercontent.com"
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <NextUIProvider theme={darkTheme}>
+        <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+            <App />
+        </GoogleOAuthProvider>
+    </NextUIProvider>
+)
+
+// Old version:
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
