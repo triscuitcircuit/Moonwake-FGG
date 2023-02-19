bfa0bad70e45bc3988d8403e32cec3dba5eb9ce7
Cedric-Fahey <98495434+Cedric-Fahey@users.noreply.github.com>
2023-02-19 15:29:31 -0500

    Added AssociationReadMe and worked on associations.

--- fgg-back-end/src/Database/config/assoc.js
+++ fgg-back-end/src/Database/config/assoc.js
@@ -3,16 +3,14 @@ const Sequelize = require('sequelize');
 function applyExtraSetup(sequelize) {
     const { AB_ATTRIBUTE, CHLE_CHALLENGE_LEVEL,
         GASYMO_GAME_SYSTEM_MONSTER, MOTY_MONSTER_TYPE, SZ_SIZE, ABCA_ATTRIBUTE_CATEGORY
-        , GACO_GAME_COMPANY, MOAB_MONSTER_ATTRIBUTE} = sequelize.models;
+        , GACO_GAME_COMPANY, MOAB_MONSTER_ATTRIBUTE, MO_MONSTER} = sequelize.models;
 
 
     AB_ATTRIBUTE.hasOne(ABCA_ATTRIBUTE_CATEGORY,{
         foreignKey: 'ABCA_ID',
         sourceKey: "ABCA_ID"})
 
-    GASYMO_GAME_SYSTEM_MONSTER.hasMany(MOAB_MONSTER_ATTRIBUTE,
-        {foreignKey: 'GASYMO_ID'})
-
+    //GASYMO GAME SYSTEM MONSTER
 
     GASYMO_GAME_SYSTEM_MONSTER.hasOne(CHLE_CHALLENGE_LEVEL,
         {foreignKey: {
@@ -21,6 +19,25 @@ function applyExtraSetup(sequelize) {
             },
             sourceKey: 'CHLE_ID'
         })
+
+    GASYMO_GAME_SYSTEM_MONSTER.hasOne(GACO_GAME_COMPANY,
+        {foreignKey: {
+                name:"GACO_ID",
+                allowNull: false
+            },
+            sourceKey: 'GACO_ID'
+        })
+    // TABLE IS NOT COMPLETE DO NOT INCLUDE
+    // GASYMO_GAME_SYSTEM_MONSTER.hasOne(MO_MONSTER,
+    //     {foreignKey: {
+    //             name:"MO_ID",
+    //             allowNull: false
+    //         },
+    //         sourceKey: 'MO_ID'
+    //     })
+
+
+
     GASYMO_GAME_SYSTEM_MONSTER.hasOne(MOTY_MONSTER_TYPE,
         {foreignKey: {
                 name:"MOTY_ID",
@@ -36,13 +53,12 @@ function applyExtraSetup(sequelize) {
             },
             sourceKey: 'SZ_ID'
         })
-    GASYMO_GAME_SYSTEM_MONSTER.hasOne(GACO_GAME_COMPANY,
-        {foreignKey: {
-                name:"GACO_ID",
-                allowNull: false
-            },
-            sourceKey: 'GACO_ID'
-        })
+
+// Saw in GASYMO_ID
+    GASYMO_GAME_SYSTEM_MONSTER.hasMany(MOAB_MONSTER_ATTRIBUTE,
+        {foreignKey: 'GASYMO_ID'})
+
+
 
     CHLE_CHALLENGE_LEVEL.belongsTo(GASYMO_GAME_SYSTEM_MONSTER,
         {foreignKey:"CHLE_ID"})
