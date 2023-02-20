985bc9655dcf0a009c4601a2cb3bd9e45db3205f
triscuitcircuit <54428989+Triscuit-circuit@users.noreply.github.com>
2023-02-20 05:25:54 -0500

    Added Pagination for connecting to game system monster, added route test, added pagintation helper functions, added creature description from database.

--- fgg-back-end/src/Database/config/helpers.js
+++ fgg-back-end/src/Database/config/helpers.js
@@ -7,5 +7,25 @@ function getIdParam(req) {
     }
     throw new TypeError(`Invalid ':id' param: "${id}"`);
 }
+// Helper function to determine the limit and page amount for limiting amount of data
+// for the front-end
+const getPagination = (page, size) => {
+    const limit = size ? +size : 25;
+    const offset = page ? page * limit : 0;
 
-module.exports = { getIdParam };
+    return { limit, offset };
+};
+
+// Helper function to determine the limit and page amount for limiting amount of data
+// for the front-end
+const getPagingData = (data, page, limit) => {
+    const { count: totalItems, rows: monsters } = data;
+    const currentPage = page ? +page : 0;
+    const totalPages = Math.ceil((totalItems/200) / limit);
+
+    return { totalItems, monsters, totalPages, currentPage };
+};
+
+
+
+module.exports = { getIdParam , getPagination, getPagingData};
