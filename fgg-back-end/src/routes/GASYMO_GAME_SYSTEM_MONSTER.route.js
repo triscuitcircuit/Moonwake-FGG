435ed3f692ea5138e50fdae170078dd6fedea4bf
Matthew Virgin <42557195+mvirgin@users.noreply.github.com>
2023-04-08 04:04:20 -0400

    progress with associated forced AND issue

--- fgg-back-end/src/routes/GASYMO_GAME_SYSTEM_MONSTER.route.js
+++ fgg-back-end/src/routes/GASYMO_GAME_SYSTEM_MONSTER.route.js
@@ -59,17 +59,17 @@ async function getAll(req, res) {
 
     const m_ac = req.query.m_ac;
     let m_ac_arr = null;
-    if (m_ac && m_ac != "0,99") {
+    if (m_ac && m_ac !== "0,99") {
         m_ac_arr = m_ac.split(",");   // turns AC selection into [min AC, max AC]
     }
     const hp = req.query.hp;
     let hp_arr = null;
-    if (hp && hp != "0,99") {                       // TODO: ignore also if range is 0,99?
+    if (hp && hp !== "0,99") {                       // TODO: ignore also if range is 0,99?
         hp_arr = hp.split(",");
     }
     const str = req.query.str;
     let str_arr = null;
-    if (str && str != "0,99") {
+    if (str && str !== "0,99") {
         str_arr = str.split(",");
     }
 
@@ -86,6 +86,11 @@ async function getAll(req, res) {
         {all: true, nested: true}
     ];
 
+    let strCol;
+    if (str){
+        strCol = Sequelize.col('MOAB_MONSTER_ATTRIBUTES.MOAB_DISPLAY_TEXT');
+    }
+
     // put stuff the user is searching for here, use ternary operators to handle them being null when not searched for
     // unless value being searched for is an associated attribute - see below
     // (list of ternary operators)
@@ -94,7 +99,7 @@ async function getAll(req, res) {
         // (ex): find the results of Ape OR ape OR APE OR aPe, etc
         name1 ? { [Op.or]: generateCapitalizations(name1)} : null,     // OR's together the list of names from the func
         m_ac ? { GASYMO_ARMOR_CLASS : {[Op.between]: [m_ac_arr[0], m_ac_arr[1]]} } : null,
-        str ? {"$MOAB_MONSTER_ATTRIBUTE.MOAB_DISPLAY_TEXT$": {[Op.substring]: "STR " + str_arr[0]}} : null // todo figure out how to get associated value properly:  "MOAB_MONSTER_ATTRIBUTE"."MOAB_DISPLAY_TEXT": invalid identifier
+        str ? {'MOAB_MONSTER_ATTRIBUTES.MOAB_DISPLAY_TEXT' : {[Op.substring]: "STR " + str_arr[0]}} : null // todo figure out how to get associated value properly:  "MOAB_MONSTER_ATTRIBUTE"."MOAB_DISPLAY_TEXT": invalid identifier
       //  str ? {'$MOAB_MONSTER_ATTRIBUTEs.MOAB_DISPLAY_TEXT$': {[Op.substring]: "STR " + str_arr[0]}} : null// add str here - get include statement correct
       //  str ? Sequelize.literal('"MOAB_MONSTER_ATTRIBUTE"."MOAB_DISPLAY_TEXT" = \'STR 16(+3)\'') : null
     ];
@@ -107,6 +112,13 @@ async function getAll(req, res) {
         where[Op.and] = content;
     }
 
+    // if (str) {
+    //     s_include.push({
+    //             model: db.sequelize.models.MOAB_MONSTER_ATTRIBUTE,
+    //             attributes: ['MOAB_DISPLAY_TEXT']
+    //         })
+    //     //where.MOAB_MONSTER_ATTRIBUTE = { MOAB_DISPLAY_TEXT: {[Op.like]: "%STR 16(+3)%"}};
+    // }
     // attributes outside of GASYMO_GAME_SYSTEM_MONSTER:
     // TODO: these force AND when they shouldn't when user is searching for them
     // meaning the outside attributes take priority and will ignore what the user has set for the inside ones
@@ -177,12 +189,29 @@ async function getAll(req, res) {
     // else just add the and or or? and add st code in by itself?
 const GASYMO_GAME_SYSTEM_MONSTER =
         await db.sequelize.models.GASYMO_GAME_SYSTEM_MONSTER.findAndCountAll({
-            limit,
-            offset,
-            include: s_include,
-            where: where
+            // limit,
+            // offset,
+            // where: where,
+            // include: s_include
+            where: {
+                [Op.or]: [
+                    { GASYMO_DISPLAY_NAME: 'Goblin' },
+                    Sequelize.where(Sequelize.col('MOAB_MONSTER_ATTRIBUTEs.MOAB_DISPLAY_TEXT'), 'STR 8(-1)') //LOOKS LIKE THIS WORKED!!! But problem with how creature-database.tsx gets everything ... work around here or there?
+                ]
+            },
+            include: [
+                {all: true, nested: true},
+                {
+                    model: db.sequelize.models.MOAB_MONSTER_ATTRIBUTE,
+                    attributes: []
+                }
+            ],
+            attributes: [
+                'GASYMO_DISPLAY_NAME',
+                [Sequelize.col('MOAB_MONSTER_ATTRIBUTEs.MOAB_DISPLAY_TEXT'), 'MOAB_DISPLAY_TEXT']
+            ]
         });
-
+console.log("WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",GASYMO_GAME_SYSTEM_MONSTER);
     res.status(200).send(getPagingData(GASYMO_GAME_SYSTEM_MONSTER, page, limit))
 }
 
